f91ee37d355bc725624f2826635dca179ebeb5fa
yu <yu@yudeMacBook-Pro.local>
2017-02-06 08:44:00 +0800

    reg-login

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var userModel = require('../model/users');
-
+var utils = require('../utils');
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-    res.render('index', { title: '登录' });
+router.get('/login', function (req, res, next) {
+    res.render('users/login', {title: '登录'});
 });
-router.get('/reg', function(req, res, next) {
-    res.render('users/reg', { title: '注册' });
-});
-router.post('/reg',function(req,res,next) {
-    req.body.password = require('../utils').md5(req.body.password);
-    userModel.create(req.body,function(err,doc){
-      console.log(doc);
-      res.redirect('/users/login');
+router.post('/login', function (req, res, next) {
+    var user = req.body;
+    var username = user.username;
+    var password = utils.md5(user.password);
+    userModel.findOne({username: username, password: password}, function (err, doc) {
+        if (err) {
+            res.redirect('back');
+        } else {
+            if (doc) {
+                console.log('登录成功');
+            } else {
+                res.redirect('back');
+            }
+        }
     });
 });
-router.get('/logout', function(req, res, next) {
-    res.render('index', { title: '退出' });
+router.get('/reg', function (req, res, next) {
+    res.render('users/reg', {title: '注册'});
+});
+router.post('/reg', function (req, res, next) {
+    req.body.password = utils.md5(req.body.password);
+    userModel.create(req.body, function (err, doc) {
+        console.log(doc);
+        res.redirect('/users/login');
+    });
+});
+router.get('/logout', function (req, res, next) {
+    res.render('index', {title: '退出'});
 });
 module.exports = router;
