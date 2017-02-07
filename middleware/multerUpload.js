35c80a034104f018d7000ab23a1ec001be96b865
yu <yu@yudeMacBook-Pro.local>
2017-02-07 08:44:32 +0800

    上传封装中间件

--- middleware/multerUpload.js
+++ middleware/multerUpload.js
@@ -1,3 +1,18 @@
 /**
  * Created by yu on 2017/2/7.
  */
+//引入multer处理上传图片
+var multer = require('multer');
+var storage = multer.diskStorage({
+    //指定上传路径
+    destination: function (req, file, cb) {
+        //注意路径，位置不能写错，文件夹要存在，不然都会报错
+        cb(null, 'public/uploads');
+    },
+    //指定上传文件名
+    filename: function (req, file, cb) {
+        cb(null, Date.now()+'.'+file.mimetype.slice(file.mimetype.indexOf('/')+1));
+    }
+})
+
+module.exports = multer({ storage: storage });
--- routes/article.js
+++ routes/article.js
@@ -1,21 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../middleware/auth');//引入权限验证中间件
-//引入multer处理上传图片
-var multer = require('multer');
-var storage = multer.diskStorage({
-    //指定上传路径
-    destination: function (req, file, cb) {
-        //注意路径，位置不能写错，文件夹要存在，不然都会报错
-        cb(null, 'public/uploads');
-    },
-    //指定上传文件名
-    filename: function (req, file, cb) {
-        cb(null, Date.now()+'.'+file.mimetype.slice(file.mimetype.indexOf('/')+1));
-    }
-})
-
-var upload = multer({ storage: storage });
+var upload = require('../middleware/multerUpload');//引入封装好的上传中间件
 
 
 //获得上传功能的中间件方法 upload
